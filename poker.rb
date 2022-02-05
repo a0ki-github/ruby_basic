e2038d1caa0b6422b75ed51b6837b380b7f2fd2b
a0ki-github <aoki.hotaka@gmail.com>
2022-02-05 09:29:34 +0900

    Add: 課題5 ポーカー 完了
    所要時間：約2時間
    メモ：
    カードをマークと数字に分けるのに約20分
    マークと数字がそれぞれいくつ重複しているのか取得する方法を探すのに約1時間
    連番になっているか確認する方法を探すのに約20分
    あとはClassの使用要否について迷ったり、なるべく綺麗に書こうとする時間に費やしたと思います。

---
+++ poker.rb
@@ -0,0 +1,98 @@
+class Hand
+  def initialize
+    @cards = ARGV
+  end
+
+  def open
+    result =
+      if royal_straight_flash?
+        'ロイヤルストレートフラッシュ'
+      elsif straight_flash?
+        'ストレートフラッシュ'
+      elsif four_card?
+        'フォーカード'
+      elsif full_house?
+        'フルハウス'
+      elsif flash?
+        'フラッシュ'
+      elsif straight?
+        'ストレート'
+      elsif three_card?
+        'スリーカード'
+      elsif two_pair?
+        'ツーペア'
+      elsif one_pair?
+        'ワンペア'
+      else
+        'ノーハンド'
+      end
+
+    puts result
+  end
+
+  private
+
+    def royal_straight_flash?
+      all_same_suits? && numbers.sort == [1, 10, 11, 12, 13]
+    end
+
+    def straight_flash?
+      all_same_suits? && serial_numbers?
+    end
+
+    def four_card?
+      numbers_configuration == [4, 1]
+    end
+
+    def full_house?
+      numbers_configuration == [3, 2]
+    end
+
+    def straight?
+      serial_numbers?
+    end
+
+    def flash?
+      all_same_suits?
+    end
+
+    def three_card?
+      numbers_configuration == [3, 1, 1]
+    end
+
+    def two_pair?
+      numbers_configuration == [2, 2, 1]
+    end
+
+    def one_pair?
+      numbers_configuration == [2, 1, 1, 1]
+    end
+
+    def all_same_suits?
+      suits_configuration == [5]
+    end
+
+    def serial_numbers?
+      numbers.max - numbers.min == 4
+    end
+
+    def suits_configuration
+      suits.group_by(&:itself).map{|key, value| value.count}.sort.reverse
+    end
+
+    def numbers_configuration
+      numbers.group_by(&:itself).map{|key, value| value.count}.sort.reverse
+    end
+
+    def suits
+      @cards.map{|card| card[0]}
+    end
+
+    def numbers
+      @cards.map{|card| card[1..2].to_i}
+    end
+end
+
+hand = Hand.new
+hand.open
+
