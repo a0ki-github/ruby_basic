6cf5ffd037a074072f91fab55e1499a9a4c91d94
a0ki-github <aoki.hotaka@gmail.com>
2022-02-19 07:01:30 +0900

    Add: quick_sort.rbを作成

---
+++ quick_sort.rb
@@ -0,0 +1,13 @@
+class Array
+  def quick_sort
+    return self if self.count <= 1
+
+    std = self.shift
+    smaller = self.select{|n| std > n}
+    bigger = self.select{|n| n > std}
+
+    return smaller.quick_sort + [std] + bigger.quick_sort
+  end
+end
+
+p ARGV.quick_sort
