18e36e322e679c9ff85e1cd69cf55a5834e2335c
a0ki-github <aoki.hotaka@gmail.com>
2022-02-19 18:42:39 +0900

    Add: 課題2で、Array#combinationを使わない方法を追記

--- combi.rb
+++ combi.rb
@@ -1,2 +1,12 @@
 array = [*1..ARGV[0].to_i]
 p array.combination(2).to_a
+
+# Array#combinationを使わない場合
+result = []
+array.each_with_index do |_, index|
+  (array.count - index - 1).times do |n|
+    result << [array[index], array[index + n + 1]]
+  end
+end
+
+p result
